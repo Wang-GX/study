00f554192dda6751bffcf37d5e16f484d21c3464
asus <s523373295@163.com>
2020-04-27 00:54:29 +0800

    Eureka集群搭建完毕

--- eureka02/src/main/java/com/wgx/study/eureka02/Eureka02Application.java
+++ eureka02/src/main/java/com/wgx/study/eureka02/Eureka02Application.java
@@ -2,9 +2,11 @@ package com.wgx.study.eureka02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Eureka02Application {
 
